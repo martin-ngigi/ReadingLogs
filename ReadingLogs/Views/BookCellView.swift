8d60ec5acd82c5047e3825104e13971e3ef044da
martin-ngigi <martinwainaina001@gmail.com>
2024-01-28 08:32:31 +0300

    Added functionality to navigate and edit book details

--- ReadingLogs/Views/BookCellView.swift
+++ ReadingLogs/Views/BookCellView.swift
@@ -11,20 +11,25 @@ struct BookCellView: View {
     let book: Book
     
     var body: some View {
-        VStack(alignment: .leading){
-            Text(book.title)
-                .bold()
-            
-            HStack{
-                Text("**Author**: \(book.author)")
+        NavigationLink(value: book){
+            VStack(alignment: .leading){
+                Text(book.title)
+                    .bold()
                 
-                Spacer()
-                
-                Text("**Pusblished on**: \(book.publishedYear.description)")
+                HStack{
+                    Text("**Author**: \(book.author)")
+                    
+                    Spacer()
+                    
+                    Text("**Pusblished on**: \(book.publishedYear.description)")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                .padding(.top, 20)
             }
-            .font(.subheadline)
-            .foregroundStyle(.secondary)
-            .padding(.top, 20)
+        }
+        .navigationDestination(for: Book.self) { book in
+            BookDetailView(book: book)
         }
     }
 }
