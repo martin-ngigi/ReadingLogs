6ddffeaacea7cb0a552cfaae2e6924b98a5ff126
martin-ngigi <martinwainaina001@gmail.com>
2024-01-28 07:00:56 +0300

    Initial commit

---
+++ ReadingLogs/ReadingLogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingLogsApp.swift
+//  ReadingLogs
+//
+//  Created by Martin Wainaina on 28/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReadingLogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
